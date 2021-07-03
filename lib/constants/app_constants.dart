5cea254de64b63852e5c987fe13bee606b2d042f
Khushi-Agrawal-1 <khushiagrawal2019@vitstudent.ac.in>
2021-07-04 01:57:10 +0530

    done

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#4D1A7F");
+final Color accentHexColor = HexColor("#FCC91C");
